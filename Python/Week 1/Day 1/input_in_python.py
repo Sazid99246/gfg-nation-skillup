afdd652758081577d6ac2603439186c2bc01112e
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-07-19 21:20:15 +0600

    Day 1 of Python Course completed and some of day 2

---
+++ Python/Week 1/Day 1/input_in_python.py
@@ -0,0 +1,18 @@
+#User function Template for python3
+########### Write your code below ###############
+
+# Take string input and print the string input
+s = input()
+print(s)
+
+# Take integer input and add 10 to the integer input and print
+n = int(input())
+n += 10
+print(n)
+
+# Take floating-point input and multiply the float input by 10 and print
+f = float(input())
+f *= 10
+print(f)
+
+########### Write your code above ###############
