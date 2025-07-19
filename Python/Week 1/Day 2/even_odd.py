439aff9533bcddf382f2b94312b4028b48212d85
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2025-07-19 23:25:33 +0600

    Week 1 Day 2 of Python is completed

--- Python/Week 1/Day 2/even_odd.py
+++ Python/Week 1/Day 2/even_odd.py
@@ -1,9 +1,2 @@
 def checkOddEven(x):
-    if (x % 2 == 0):
-        # Complete the statement below
-        return "Even"
-
-    else:
-        # Complete the statement below
-        return "Odd"
-
+    return x % 2 == 0
